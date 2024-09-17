ad90a2185f6e14f83fa128cec16665f7ea4882af
Feodor Fitsner <feodor@appveyor.com>
2024-09-17 13:50:49 -0700

    iOS packaging complete

--- package-ios-for-dart.sh
+++ package-ios-for-dart.sh
@@ -27,23 +27,24 @@ mkdir -p $frameworks_dir
 mkdir -p $stdlib_dir
 
 # copy Python.xcframework
-rsync -av $python_apple_support_root/support/$python_version_short/iOS/Python.xcframework $frameworks_dir
+rsync -av --exclude-from=$script_dir/python-darwin-framework.exclude $python_apple_support_root/support/$python_version_short/iOS/Python.xcframework $frameworks_dir
 cp -r $script_dir/Modules $frameworks_dir/Python.xcframework/ios-arm64/Python.framework
 cp -r $script_dir/Modules $frameworks_dir/Python.xcframework/ios-arm64_x86_64-simulator/Python.framework
 
 # copy stdlibs
 for arch in "${archs[@]}"; do
-    rsync -av --exclude-from=$script_dir/python-darwin-dart.exclude $python_apple_support_root/install/iOS/$arch/python-*/lib/python$python_version_short/* $stdlib_dir/$arch
+    rsync -av --exclude-from=$script_dir/python-darwin-stdlib.exclude $python_apple_support_root/install/iOS/$arch/python-*/lib/python$python_version_short/* $stdlib_dir/$arch
 done
 
 echo "Converting lib-dynload to xcframeworks..."
-find "$stdlib_dir/${archs[0]}/lib-dynload" -name "*.$dylib_suffix" | while read full_dylib; do
+find "$stdlib_dir/${archs[0]}/lib-dynload" -name "*.$dylib_ext" | while read full_dylib; do
     dylib_relative_path=${full_dylib#$stdlib_dir/${archs[0]}/lib-dynload/}
     create_xcframework_from_dylibs \
         "$stdlib_dir/${archs[0]}/lib-dynload" \
         "$stdlib_dir/${archs[1]}/lib-dynload" \
         "$stdlib_dir/${archs[2]}/lib-dynload" \
         $dylib_relative_path \
+        "Frameworks/serious_python_darwin.framework/python-stdlib/lib-dynload" \
         $frameworks_dir
     #break # run for one lib only - for tests
 done
@@ -55,12 +56,11 @@ for arch in "${archs[@]}"; do
     find $stdlib_dir/$arch -name _sysconfigdata__*.py -exec cp {} $stdlib_dir \;
     rm -rf $stdlib_dir/$arch
 done
-rm -rf $stdlib_dir/lib-dynload
 
 # compile stdlib
 cd $stdlib_dir
 python -m compileall -b .
-find . \( -name '*.so' -or -name "*.$dylib_suffix" -or -name '*.py' -or -name '*.typed' \) -type f -delete
+find . \( -name '*.so' -or -name "*.$dylib_ext" -or -name '*.py' -or -name '*.typed' \) -type f -delete
 rm -rf __pycache__
 rm -rf **/__pycache__
 cd -
